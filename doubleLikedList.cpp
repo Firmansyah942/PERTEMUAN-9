e0fd9f1c997e24990b9853cce7fcd3ed2c6b39ec
Firmansyah942 <firmnsyh318@gmail.com>
2025-05-15 10:49:52 +0700

    membuat class node dan class double linked list

---
+++ doubleLikedList.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class node
+{
+    public:
+        int noMhs;
+        Node*next;
+        Node*prev;
+    
+};
+
+class doubleLinkList
+{
+    private:
+        Node*START;
+    public:
+    doubleLinkedlist()    
+    {
+        START = NULL;
+    }
+
+}
